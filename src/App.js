dcf0699eb5160c4cfc3da96f2db2ba9ea36efa6b
MOHANA4049 <mohanarajendran37@gmail.com>
2023-04-18 16:00:24 +0530

    QuoteGenerator

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+class App extends React.Component {
+
+	state = { advice: '' };
+
+	componentDidMount() {
+		this.fetchAdvice();
+	}
+
+	fetchAdvice = () => {
+		axios.get('https://api.adviceslip.com/advice')
+		.then((response) => {
+			const { advice } = response.data.slip;
+
+			this.setState({ advice });
+		})
+
+		.catch ((error) => {
+			console.log(error);
+		})
+	}
+
+	render() {
+		const { advice } = this.state;
+
+		return (
+			<div className="app">
+				<div className="card">
+					<h1 className="heading">
+					{this.state.advice}
+					</h1>
+				</div>
+        <button className="button" onClick=
+					{this.fetchAdvice}>
+						<h2>NEXT</h2>
+					</button>
+			</div>
+		);
+	}
+}
+
+export default App;
